8f1df15689a569d7a864542d3e400bdaf9d14541
Mithin <13951154+mithintv@users.noreply.github.com>
2023-02-27 14:23:36 -0500

    debugging

--- playground/views.py
+++ playground/views.py
@@ -6,5 +6,11 @@ from django.http import HttpResponse
 #  request handler
 #  action
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {"name": "Mithin"})
